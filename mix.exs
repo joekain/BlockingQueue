f47ad09b84d5542fde707230bf812f5061298eff
Joseph Kain <joekain@gmail.com>
2015-10-17 21:41:36 -0700

    Bump version number 1.0.5

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule BlockingQueue.Mixfile do
 
   def project do
     [app: :blocking_queue,
-     version: "1.0.4",
+     version: "1.0.5",
      elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
